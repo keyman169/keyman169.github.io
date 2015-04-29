679ba5fec14f1557e3939d3c883a006b433f35f0
keyman169 <keyman169@163.com>
2015-04-29 08:21:13 +0800

    grid

--- js/game.js
+++ js/game.js
@@ -1,8 +1,22 @@
 
-function generateTile(occupted){
-
+function generateTile(exist){
+    var tile = document.createElement("div");
+    tile.innerHTML = "abc";
+    return tile;
 }
 
-function generateGrid() {
-
+function generateGrid(map) {
+    var table = document.getElementById("container");
+    for (var j=0;j<map.height;j++){
+        var tr = document.createElement('tr');
+        for(var i=0;i<map.width;i++){
+            var td = document.createElement('td');
+            var exist = map.item[j*map.width  + i];
+            td.appendChild(generateTile());
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
 }
+
+generateGrid(mapDate.simple);
