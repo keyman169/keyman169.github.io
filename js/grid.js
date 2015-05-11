7ad410faff1d4114e7381ef01ae33ed91a2c3048
keyman169 <keyman169@163.com>
2015-05-11 15:15:42 +0800

    dd

--- js/grid.js
+++ js/grid.js
@@ -5,12 +5,15 @@ function Grid(){
 
 }
 
-Grid.prototype.createTile = function(exist){
+Grid.prototype.createTile = function(exist,i,j){
     var tile = document.createElement("div");
-    var classes = ["grid"];
+    var classes = ["tile"];
     tile.setAttribute("class", classes.join(" "));
+    tile.setAttribute("ondrop","drop(event)");
+    tile.setAttribute("ondragover","allowDrop(event)");
     if(exist>1){
-        tile.innerHTML = "<img src='image/tile.png' width='60' height = '60' draggable='true' >";
+        var tileId = "img-" + i + "-" + j;
+        tile.innerHTML ="<img src='image/tile.png' width='60' height = '60' draggable='true' ondragstart='drag(event)' id=" + tileId+ "\/>";
     }
     return tile;
 };
@@ -24,10 +27,24 @@ Grid.prototype.init  = function (map) {
             var td = document.createElement('td');
             var exist = map.item[j*map.width  + i];
             if(exist>0){
-                td.appendChild(this.createTile(exist));
+                td.appendChild(this.createTile(exist,i,j));
             }
             tr.appendChild(td);
         }
         table.appendChild(tr);
     }
+};
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
 }
